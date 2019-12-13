a6db9321d9ccfe0e9fae22f58e8659cb4ad7a860
Stypulkowski <piotr.stypulkowski@accenture.com>
2019-12-13 15:53:38 +0100

    update

--- calc.py
+++ calc.py
@@ -1,4 +1,9 @@
-#calc prototype ver. beta
+# calc prototype ver. beta
+
+'''
+This is a multiline
+comment.
+'''
 
 number_1 = int(input('Enter your first number: '))
 number_2 = int(input('Enter your second number: '))
