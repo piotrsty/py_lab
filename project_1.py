da2a92f419d6c9093c74dc5829dee7fda428f3db
Stypulkowski, Piotr <piotr.stypulkowski@accenture.com>
2021-02-09 00:56:51 +0100

    project no 1 by mosh

--- project_1.py
+++ project_1.py
@@ -1,5 +1,9 @@
-import openpyxl as xl 
-wb = xl.load_workbook('transactions.xlsx')
+import openpyxl as xl
+from openpyxl.chart import BarChart, Reference
+
+# wywalic funkcje dla jasnego obrazu, bo nie dziala !!!
+def process_workbook(filename):
+    wb = xl.load_workbook(filename)
 sheet = wb['Sheet1']
 #cell = sheet['a1']
 cell = sheet.cell(1, 1)
@@ -13,6 +17,14 @@ for row in range(2,sheet.max_row + 1):
     corrected_price_cell.value = corrected_price
     #print(corrected_price)
 
+values = Reference(sheet, min_row=2, max_row=sheet.max_row, min_col = 4, max_col=4)
 
-wb.save('tansactions2.xlsx')
+chart = BarChart()
+chart.add_data(values)
+sheet.add_chart(chart, 'e2')
+
+wb.save(filename)
+
+
+
 
